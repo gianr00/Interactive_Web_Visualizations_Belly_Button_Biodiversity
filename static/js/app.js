e4f79cb0f0977a7d5c24d1061b2bdc429edf78ee
R G <gianr00@gmail.com>
2022-09-23 16:22:28 -0400

    dropdown data

---
+++ static/js/app.js
@@ -0,0 +1,44 @@
+
+const url_samples_data = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+
+function init() {
+    
+
+    d3.json(url_samples_data).then(function(data) {
+        console.log(data);
+    
+        let data_names    = Object.values(data.names)
+        let data_metadata = Object.values(data.metadata)
+        let data_samples  = Object.values(data.samples)
+    
+        //log the names,  metadata and samples from data object
+        console.log("data_names   : ", data_names)
+        console.log("data_metadata: ", data_metadata)
+        console.log("data_samples : ", data_samples)
+
+        let dropdownMenu = d3.select("#selDataset");
+
+        // dropdown data
+        for (let name of data_names) {
+            console.log("name: ", name);
+            dropdownMenu.append('option').text(name).property('value',name);
+        }
+
+        // demographics using first name
+        let initial_test_subject_id = data_names[0];
+        demographics (initial_test_subject_id);
+
+
+    });
+};
+
+init();
+
+    
+
+
+
+
+
+
